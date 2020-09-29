fcca68f7f726289df99027adf457251cfb5d8820
almoeeni <almoiniefazal@live.co.uk>
2020-09-29 13:33:07 +0500

    listing

--- app/admin/src/Controllers/Test/Add.php
+++ app/admin/src/Controllers/Test/Add.php
@@ -144,7 +144,7 @@ class Add extends AbstractAdminController {
         $db = $this->app->db()->primary();
         $authors = $db->query()->table(TestTable::NAME)->fetch();
         echo "<pre>";
-        var_dump($authors->array());
+        var_dump($authors->all());
         exit;
         $this->page()->title('Author Listing')->index(610, 20)
             ->prop("icon", "mdi mdi-account-plus-outline");
